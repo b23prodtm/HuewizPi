827b203346aa35b542683690210012340c97d78c
Berry by Tiana Rakoto <b23prodtm@users.sf.net>
2019-11-16 20:41:06 +0000

    Added auto-reboot PR#1

---
+++ scripts/init.d/auto-reboot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# auto-reboot
+# description: Check if the system dpkg need a restart and send corresponding signal
+start() {
+# code to start app comes here 
+# example: bahs -c "program_name" &
+    bash -c "while [ ! -f /var/run/reboot-required ]; do sleep 30; done; cat /var/run/reboot-required.dpkgs 2> /dev/null; reboot" &
+echo $! > /var/run/chkdpkgs.pid
+}
+stop() {
+# code to stop app comes here 
+# example: kill program_name.pid
+kill $(cat /var/run/chkdpkgs.pid) 
+    rm /var/run/chkdpkgs.pid
+}
+status() {
+if [ -e /var/run/chkdpkgs.pid ]; then
+echo auto-reboot is running, pid=$(cat /var/run/chkdpkgs.pid)
+else
+echo auto-reboot is NOT running
+exit 1
+fi
+}
+case "$1" in 
+    start)
+       start
+       ;;
+    stop)
+       stop
+       ;;
+    restart)
+       stop
+       start
+       ;;
+    status)
+# code to check status of app comes here 
+# example: status program_name
+       status 
+      ;;
+*)
+echo "Usage: $0 {start|stop|status|restart}"
+esac
+exit 0 
--- scripts/init.d/net_restart.sh
+++ scripts/init.d/net_restart.sh
@@ -38,6 +38,11 @@ ${MARKER_END}\\n'/ /etc/rc.local"
 logger -st sed "/etc/rc.local added command lines"
    cat /etc/rc.local
 fi
+logger -st dpkg "installing dpkg auto-reboot.service"
+sudo cp -f ${work_dir}init.d/auto-reboot.sh /usr/local/bin/auto-reboot.sh
+sudo chmod +x /usr/local/bin/auto-reboot.sh
+sudo cp -f ${work_dir}init.d/auto-reboot.service /etc/systemd/system/auto-reboot.service
+sudo systemctl enable auto-reboot
 logger -st ufw  "enable ip forwarding (internet connectivity)"
 source ${work_dir}init.d/init_ufw.sh
 case $REBOOT in
