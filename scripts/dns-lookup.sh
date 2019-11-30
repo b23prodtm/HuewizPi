249d21e8b4c939f5db417caf30ef5c013ddd6123
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2019-11-30 11:35:51 +0100

    Exec script

--- scripts/dns-lookup.sh
+++ scripts/dns-lookup.sh
@@ -8,4 +8,12 @@ else # mac_os
   dns1=$(scutil --dns | grep "nameserver\[.\] :" | awk '/([0-9]*\.){3}/{print $3}')
   # | head -n 1)
 fi
-printf "Here follows DNS address:\n  %s\nLet's run for it.\n" $dns1
+s="ip: %s\n"
+while [ "$#" -gt 0 ]; do case $1 in
+  -s*)
+    s="%s\n";;
+  * );;
+esac; shift; done
+printf "Here follow DNS addresses entries:\n"
+printf "$s" $dns1
+printf "Let's run for it.\n"
