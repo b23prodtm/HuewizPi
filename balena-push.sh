24e4bb70e0035d2f29ebb758328198a313ec13b8
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2019-12-14 18:01:06 +0100

    amd64 arch to push

--- balena-push.sh
+++ balena-push.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 eval $(ssh-agent)
 ssh-add ~/.ssh/*id_rsa
+source ./configure-docker-arch.sh
+git commit -a -m "${DKR_ARCH} pushed to balena.io"
 git push -uf balena $*
--- configure-docker-arch.sh
+++ configure-docker-arch.sh
@@ -36,14 +36,13 @@ ln -vsf ${arch}.env .env
 eval $(cat ${arch}.env)
 function setArch() {
   while [ "$#" -gt 1 ]; do
+    cp -f $1 $1.old
     sed -E -e s/"%%BALENA_MACHINE_NAME%%"/"${BALENA_MACHINE_NAME}"/ \
     -e "s/\\\$DKR_ARCH/${DKR_ARCH}/g" \
     -e "s/(Dockerfile\.)[^\.]*/\\1${DKR_ARCH}/g" \
     -e "/image:/s/(betothreeprod[^\-]*).*/\\1-${DKR_ARCH}/g" \
-    $1 | tee "$2.new"
-    cp -f $1 $1.old
-    mv -f $2.new $2
+    $1 | tee $2
   shift; shift; done
 }
-setArch docker-compose.yml docker-compose.yml Dockerfile.template Dockerfile.${DKR_ARCH}
+setArch docker-compose.yml docker-compose.${DKR_ARCH} Dockerfile.template Dockerfile.${DKR_ARCH}
 eval $(cat ${arch}.env | grep BALENA_MACHINE_NAME)
