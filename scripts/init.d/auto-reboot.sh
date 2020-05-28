f3fb39aa307ffb28ba043f48425adf0145797c8c
TR BERRY <b23prodtm@sourceforge.net>
2020-05-28 11:57:37 +0000

    Fixes: auto_reboot service

--- scripts/init.d/auto-reboot.sh
+++ scripts/init.d/auto-reboot.sh
@@ -1,64 +1,7 @@
-#!/bin/bash
-# auto-reboot
-# description: Check if the system dpkg need a restart and send corresponding signal
-[ -z ${scriptsd} ] && export scriptsd=$(cd `dirname $BASH_SOURCE`/.. && pwd)
-banner=("" "[$0] BUILD RUNNING $BASH_SOURCE" ""); printf "%s\n" "${banner[@]}"
-usage="
-Usage: $0 {start|stop|status|restart|install|uninstall}"
-install() {
-  sudo cp -f ${scriptsd}/init.d/auto-reboot.sh /usr/local/bin/auto-reboot.sh
-  sudo chmod +x /usr/local/bin/auto-reboot.sh
-  sudo cp -f ${scriptsd}/init.d/auto-reboot.service /etc/systemd/system/auto-reboot.service
-  sudo systemctl enable auto-reboot
-}
-uninstall() {
-  sudo systemctl disable auto-reboot
-  sudo rm -f /usr/local/bin/auto-reboot.sh
-  sudo rm -f /etc/systemd/system/auto-reboot.service
-}
-start() {
-# code to start app comes here
-# example: bahs -c "program_name" &
-    bash -c "while [ ! -f /run/reboot-required ]; do sleep 30; done; cat /run/reboot-required.dpkgs 2> /dev/null; reboot" &
-    echo $! > /run/chkdpkgs.pid
-}
-stop() {
-# code to stop app comes here
-# example: kill program_name.pid
-  kill $(cat /run/chkdpkgs.pid)
-  rm /run/chkdpkgs.pid
-}
-status() {
-if [ -e /run/chkdpkgs.pid ]; then
-  echo auto-reboot is running, pid=$(cat /run/chkdpkgs.pid)
-else
-  echo auto-reboot is NOT running
-  exit 1
-fi
-}
-case "$1" in
-    start)
-       start
-       ;;
-    stop)
-       stop
-       ;;
-    restart)
-       stop
-       start
-       ;;
-    status)
-# code to check status of app comes here
-# example: status program_name
-       status
-       ;;
-    install)
-       install
-       ;;
-    uninstall)
-       uninstall
-       ;;
-*)
-echo -e $usage
-esac
-exit 0
+#!/usr/bin/env bash
+### Paste in here latest File Revisions
+REV=https://raw.githubusercontent.com/b23prodtm/vagrant-shell-scripts/b23prodtm-patch/vendor/cni/auto_reboot
+sudo curl -SL -o /usr/local/lib/auto_reboot $REV
+sudo curl -SL -o /usr/local/lib/auto_reboot.service $REV.service
+sudo chmod 0755 /usr/local/lib/auto_reboot
+/usr/local/lib/auto_reboot "$@"
