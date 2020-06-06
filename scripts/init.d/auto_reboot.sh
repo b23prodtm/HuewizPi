6e2afce9cbca803ecf47afa012c308e28c11f791
Tiana Rakoto Arimanana <5566338+b23prodtm@users.noreply.github.com>
2020-06-06 18:44:53 +0200

    Fixes: auto_reboot service

--- scripts/init.d/auto_reboot.sh
+++ scripts/init.d/auto_reboot.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 ### Paste in here latest File Revisions
 REV=https://raw.githubusercontent.com/b23prodtm/vagrant-shell-scripts/b23prodtm-patch/vendor/cni/auto_reboot.sh
-[ -z "${scriptsd:-}" ] && scriptsd="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-sudo curl -SL -o "${scriptsd}/"auto_reboot $REV
-sudo curl -SL -o "${scriptsd}/"auto_reboot.service $REV.service
-sudo chmod 0755 "${scriptsd}/auto_reboot"
-"${scriptsd}/auto_reboot" "$@"
+sudo curl -SL -o /usr/local/bin/auto_reboot $REV
+sudo chmod 0755 /usr/local/bin/auto_reboot
+auto_reboot "$@"
