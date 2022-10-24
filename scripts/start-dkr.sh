c62ce32d26c555cc4cc9ab8f3199b1ac374b85a5
BNOPOD <b23prodtm@users.sourceforge.net>
2022-10-24 22:42:29 +0200

    BALENA_ARCH environment

--- scripts/start-dkr.sh
+++ scripts/start-dkr.sh
@@ -2,5 +2,5 @@
 [ -z "${scriptsd:-}" ] && scriptsd="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
 # shellcheck disable=SC1090
 . "$scriptsd/.env" && . "$scriptsd/common.env"
-balena_deploy "${BASH_SOURCE[0]}" "$DKR_ARCH" --docker 0
-docker-compose -f "docker-compose.${DKR_ARCH}" up
+balena_deploy "${BASH_SOURCE[0]}" "$BALENA_ARCH" --docker 0
+docker-compose -f "docker-compose.${BALENA_ARCH}" up
