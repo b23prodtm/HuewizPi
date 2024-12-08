96cb28fc3ce7c970638dcbae94b46aff34ec110b
Tina B Rakoto <webmaster@b23prodtm.info>
2024-12-08 12:41:18 +0100

    updated MariaDb database passbolt gets root access on balenaOS container (docker-compose) homeassistant DBUS devices

---
+++ update-templates.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+ARC=(armhf x86_64 aarch64)
+
+# Loop through the array and echo each value
+for arch in "${ARC[@]}"; do
+  printf "Updating templates, %s \n" "$arch"
+  echo "0" | ./deploy.sh "$arch" 1 2> /dev/null > /dev/null	
+done
+
+git add docker-compose.yml
+git commit -m "Updated Templates"
