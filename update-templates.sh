2c1d31445b94d8972b20bf0dc1559fc5066606e6
Tina B Rakoto <webmaster@b23prodtm.info>
2025-04-25 23:24:42 +0200

    armhf pushed ./passbolt

--- update-templates.sh
+++ update-templates.sh
@@ -4,7 +4,7 @@ ARC=(armhf x86_64 aarch64)
 # Loop through the array and echo each value
 for arch in "${ARC[@]}"; do
   printf "Updating templates, %s \n" "$arch"
-  echo "0" | ./deploy.sh "$arch" 2 2> /dev/null > /dev/null	
+  echo "0" | ./deploy.sh "$arch" 2> /dev/null > /dev/null
 done
 
 git add docker-compose.yml
